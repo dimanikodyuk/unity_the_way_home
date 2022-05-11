0ddda6e3e839df6443a2cbeb16d6d06653ef5e7a
dimanikodyuk <dimanikodyuk@gmail.com>
2022-05-11 21:33:42 +0300

    Added project

---
+++ TheWayHome/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed; // Скорость передвижения
+    [SerializeField] private float _jumpSpeed;   // Скорость/сила прыжка
+    [SerializeField] Rigidbody2D rigid;        //
+    [SerializeField] Animator _anim;           // Аниматор персонажа
+
+    private Vector2 _moveDirection;            // Направление движения
+    private bool _isFacingRight = true;        // Проверка правильно ли повёрнут персонаж
+    private bool _inAir;                       // Находится ли персонаж в воздухе
+
+
+    private bool _isGrounded;                  // Определение, каснулись ли мы земли
+    private bool _isSolidObject;
+    public Transform groundCheck;              // Позиция анализатора земли
+    private float _checkRadius = 0.15f;        // Радиус определения земли
+    public LayerMask whatIsGround;             // Какой слой является землей
+    public LayerMask whatIsSolid;             // Какой слой является землей
+
+    public int allowJumpTimes;                 // Количество доступных прыжков
+    private int _airJumpCount;                 // Счетчик прыжков
+
+
+    [SerializeField] GameObject _deathEffect;
+    [SerializeField] GameObject _jumpEffect;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>();
+        //Move(_moveDirection);
+    }
+
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!_inAir)
+        {
+            _inAir = true;
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, _checkRadius, whatIsGround);
+        _isSolidObject = Physics2D.OverlapCircle(groundCheck.position, _checkRadius, whatIsSolid);
+        if (_isGrounded || _isSolidObject)
+        {
+            _airJumpCount = 0;
+        }
+    }
+
+    private void Start()
+    {
+        _airJumpCount = 0;
+    }
+
+    private void Update()
+    {
+        Move(_moveDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        float scaleMoveSpeed = _moveSpeed * Time.deltaTime;
+        Vector3 moveDirection = new Vector3(direction.x, 0, direction.y);
+        transform.position += moveDirection * scaleMoveSpeed;
+        _anim.SetFloat("Speed", Mathf.Abs(direction.x));
+
+        if (direction.x > 0 && !_isFacingRight)
+            //отражаем персонажа вправо
+            Flip();
+        //обратная ситуация. отражаем персонажа влево
+        else if (direction.x < 0 && _isFacingRight)
+            Flip();
+    }
+
+    private void Jump()
+    {
+        if (_inAir)
+        {
+            _inAir = false;
+            if (allowJumpTimes <= 0)
+                return;
+
+            else if ((_isGrounded || _isSolidObject) && _airJumpCount == 0)
+            {
+                rigid.velocity += Vector2.up * _jumpSpeed;
+                //rigid.AddForce(Vector2.up * _jumpSpeed);
+                Instantiate(_jumpEffect, transform.position, Quaternion.identity);
+            }
+
+            else if (_airJumpCount < allowJumpTimes - 1)
+            {
+                rigid.velocity += Vector2.up * _jumpSpeed;
+                //rigid.AddForce(Vector2.up * _jumpSpeed);
+                Instantiate(_jumpEffect, transform.position, Quaternion.identity);
+                _airJumpCount++;
+            }    
+
+        }
+
+    }
+
+
+
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
